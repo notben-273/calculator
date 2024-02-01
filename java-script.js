335c10b5999d2aa95f28607b18c369bf099b2072
notben-273 <gethingben21@gmail.com>
2024-02-01 17:20:39 +0000

    Add basic math functions and set-up

---
+++ java-script.js
@@ -0,0 +1,20 @@
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function divide(number1, number2) {
+    return number1 / number2;
+}
+
+console.log(add(32, 37));
+console.log(subtract(84, 15));
+console.log(multiply(3, 23));
+console.log(divide(4761, 69));
