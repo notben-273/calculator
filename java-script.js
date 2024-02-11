b00a6d2300a9b5f04dcd1eeb2e8a51e6663bfd6c
notben-273 <gethingben21@gmail.com>
2024-02-11 12:29:50 +0000

    Add functionality to all buttons

--- java-script.js
+++ java-script.js
@@ -1,33 +1,113 @@
 const keys = document.querySelectorAll(".key");
 const display = document.querySelector("#display");
+const clearButton = document.querySelector("#ac");
+const addButton = document.querySelector("#add");
+const subtractButton = document.querySelector("#subtract");
+const multiplyButton = document.querySelector("#multiply");
+const divideButton = document.querySelector("#divide");
+const equalsButton = document.querySelector("#equals");
 
 keys.forEach(element => {
     element.addEventListener("click", (e) => {
-        //display.textContent = element.id;
+
         displayValue = displayValue + element.id;
         display.textContent = displayValue;
-    })
+    });
+});
+
+//User enters a number
+//Number is displayed on screen
+
+// Equals is entered
+// number1 is operated with the current operator with the displayValue
+// The answer becomes the new number1
+// The answer is displayed on the screen
+// The displayValue is reset
+
+// Add is entered
+// number1 and displayValue are operated with current operator
+// the answer becomes the new number1
+// the answer is displayed
+// the operator is changed
+
+clearButton.addEventListener("click", () => {
+    
+    number1 = 0;
+    operator = 'add';
+    displayValue = '';
+    display.textContent = '0';
+});
+
+equalsButton.addEventListener("click", () => {
+
+    number1 = operate(number1, displayValue, operator);
+    display.textContent = number1;
+    displayValue = '';
+});
+
+addButton.addEventListener('click', () => {
+
+    number1 = operate(number1, displayValue, operator);
+    display.textContent = number1;
+    displayValue = '';
+    operator = 'add';
+});
+
+subtractButton.addEventListener('click', () => {
+
+    number1 = operate(number1, displayValue, operator);
+    display.textContent = number1;
+    displayValue = '';
+    operator = 'subtract';
+});
+
+multiplyButton.addEventListener('click', () => {
+
+    if (displayValue == '') {
+        displayValue = '1';
+    }
+
+    number1 = operate(number1, displayValue, operator);
+    display.textContent = number1;
+    displayValue = '';
+    operator = 'multiply';
+});
+
+divideButton.addEventListener('click', () => {
+
+    if (displayValue == '') {
+        displayValue = '1';
+    }
+
+    number1 = operate(number1, displayValue, operator);
+    display.textContent = number1;
+    displayValue = '';
+    operator = 'divide';
 });
 
 function add(number1, number2) {
-    return number1 + number2;
+    return +number1 + +number2;
 }
 
 function subtract(number1, number2) {
-    return number1 - number2;
+    return +number1 - +number2;
 }
 
 function multiply(number1, number2) {
-    return number1 * number2;
+    return +number1 * +number2;
 }
 
 function divide(number1, number2) {
-    return number1 / number2;
+
+    if (number1 % number2 == 0){
+        return +number1 / +number2;
+    }
+    
+    return (+number1 / +number2).toFixed(7);
 }
 
-let number1;
-let number2;
-let operator;
+let number1 = 0;
+let operator = 'add';
 let displayValue = '';
 
 function operate(number1, number2, operator) {
@@ -43,11 +123,10 @@ function operate(number1, number2, operator) {
             return multiply(number1, number2);
 
         case "divide":
+            if (number2 == '0') {
+                return 'Nice Try';
+            }
+
             return divide(number1, number2);
     }
 }
-
-console.log(operate(33, 77, "add"));
-console.log(operate(33, 77, "subtract"));
-console.log(operate(33, 77, "multiply"));
-console.log(operate(33, 77, "divide"));
