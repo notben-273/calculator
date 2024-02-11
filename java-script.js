a528765938d557260ee5c3e8cce113537cea0eac
notben-273 <gethingben21@gmail.com>
2024-02-11 17:08:43 +0000

    Add a functional decimal point button

--- java-script.js
+++ java-script.js
@@ -6,6 +6,8 @@ const subtractButton = document.querySelector("#subtract");
 const multiplyButton = document.querySelector("#multiply");
 const divideButton = document.querySelector("#divide");
 const equalsButton = document.querySelector("#equals");
+const decimalButton = document.querySelector("#decimal");
+const backspaceButton = document.querySelector("#backspace");
 
 keys.forEach(element => {
     element.addEventListener("click", (e) => {
@@ -15,27 +17,13 @@ keys.forEach(element => {
     });
 });
 
-//User enters a number
-//Number is displayed on screen
-
-// Equals is entered
-// number1 is operated with the current operator with the displayValue
-// The answer becomes the new number1
-// The answer is displayed on the screen
-// The displayValue is reset
-
-// Add is entered
-// number1 and displayValue are operated with current operator
-// the answer becomes the new number1
-// the answer is displayed
-// the operator is changed
-
 clearButton.addEventListener("click", () => {
     
     number1 = 0;
     operator = 'add';
     displayValue = '';
     display.textContent = '0';
+    decimalEnabled = true;
 });
 
 equalsButton.addEventListener("click", () => {
@@ -43,6 +31,7 @@ equalsButton.addEventListener("click", () => {
     number1 = operate(number1, displayValue, operator);
     display.textContent = number1;
     displayValue = '';
+    operator = 'add';
 });
 
 addButton.addEventListener('click', () => {
@@ -85,6 +74,24 @@ divideButton.addEventListener('click', () => {
     operator = 'divide';
 });
 
+decimalButton.addEventListener('click', () => {
+
+    if (decimalEnabled == true) {
+        
+        if (displayValue == '') {
+
+            displayValue = "0.";
+        }
+        else {
+
+            displayValue = displayValue + '.';
+        }
+
+        display.textContent = displayValue;
+        decimalEnabled = false;
+    }
+})
+
 function add(number1, number2) {
     return +number1 + +number2;
 }
@@ -102,13 +109,14 @@ function divide(number1, number2) {
     if (number1 % number2 == 0){
         return +number1 / +number2;
     }
-    
+
     return (+number1 / +number2).toFixed(7);
 }
 
 let number1 = 0;
 let operator = 'add';
 let displayValue = '';
+let decimalEnabled = true;
 
 function operate(number1, number2, operator) {
 
